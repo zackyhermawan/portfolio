320aacdb54a4ff3eb4030491ad95e3d3c2065299
Sultan Zacky Hermawan <sultanzacky318@gmail.com>
2025-11-15 22:20:44 +0700

    fix: correct import path for Vercel

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CardWeb from '../assets/card/web'
+import CardWeb from '../assets/card/Web.js'
 import { Link } from 'react-router-dom'
 
 const Card = ({id, image, alt, title, text, tags, type }) => {
